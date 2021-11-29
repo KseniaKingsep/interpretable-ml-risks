c39648d621ce719be6a9116c8581b64f3dcb5a9b
Ksenia Kingsep <white-lioness@ya.ru>
2021-11-29 13:31:52 +0100

    comparison of SLIME and DiCE (graphs)

--- src/explainers.py
+++ src/explainers.py
@@ -5,6 +5,9 @@ from slime.lime_tabular import LimeTabularExplainer as sLimeTabularExplainer
 import matplotlib.pyplot as plt
 from src.timelimit import *
 from tqdm.notebook import tqdm
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from itertools import compress
 import seaborn as sns
 import pandas as pd
 import numpy as np
@@ -12,6 +15,8 @@ import datetime
 import dice_ml
 import random
 import pickle
+import math
+import shap
 
 import logging
 
@@ -45,7 +50,7 @@ def plot_pdp_ice(model, X_train, features, title_fill):
 
 class DiCeReport:
 
-    def __init__(self, model, desired_class):
+    def __init__(self, model, desired_class, features_to_vary):
 
         """
         :param model: model training class (trained) like CaseCI or BankMarketingModel
@@ -56,6 +61,11 @@ class DiCeReport:
         self.cfs = {}
         self.model = model
         self.desired_class = desired_class
+
+        self.features_to_vary = features_to_vary
+        if self.features_to_vary is None:
+            self.features_to_vary = self.model.names
+
         self.instances_to_change = [i for i, x in
                                     enumerate((model.grid_pipe_lgbm.predict(model.X_test) == desired_class))
                                     if x]
@@ -75,14 +85,12 @@ class DiCeReport:
         m = dice_ml.Model(model=self.model.grid_pipe_lgbm, backend='sklearn')
         self.exp = dice_ml.Dice(d, m, method)
 
-    def get_cf(self, instance_id=None, n_cf=5, features_to_vary=None,
-               timeout=5, printout=False):
+    def get_cf(self, instance_id=None, n_cf=5, timeout=5, printout=False):
 
         """
         Get counterfactuals for one instance
         :param instance_id: location in the test dataset in model
         :param n_cf: how many CGs to generate
-        :param features_to_vary: changeable features
         :param timeout: time to try CFs generation
         :param printout: print the result
         :return:
@@ -90,20 +98,16 @@ class DiCeReport:
 
         self.res = None
 
-        if features_to_vary is None:
-            features_to_vary = self.model.names
-
         if instance_id is None:
             instance_id = random.choice(self.instances_to_change)
         query_instance = self.model.X_test.iloc[[instance_id]]
 
+        # TODO try to reinitialize CFs search several times
         try:
             with time_limit(timeout):
                 self.res = self.exp.generate_counterfactuals(query_instance, total_CFs=n_cf,
                                                              desired_class="opposite", verbose=False,
-                                                             features_to_vary=features_to_vary
-                                                             )
-
+                                                             features_to_vary=self.features_to_vary)
                 if printout and self.res:
                     self.res.visualize_as_dataframe(show_only_changes=True)
 
@@ -111,8 +115,7 @@ class DiCeReport:
             self.c += 1
             pass
 
-    def evaluate_dataset(self, features_to_vary=None, n=None, save=True, name='',
-                         timeout=5, printout=False):
+    def evaluate_dataset(self, n=None, save=True, name='', timeout=5, printout=False):
 
         """
 
@@ -134,12 +137,10 @@ class DiCeReport:
         self.c = 0
         self.cfs = {}
         for row in tqdm(self.subset):
-            self.get_cf(instance_id=row, n_cf=5, features_to_vary=features_to_vary,
-                        timeout=timeout, printout=printout)
+            self.get_cf(instance_id=row, n_cf=5, timeout=timeout, printout=printout)
 
             if self.res:
                 if self.res.cf_examples_list[0].final_cfs_df is not None:
-                    # self.cfs[row] = self.res.cf_examples_list[0].final_cfs_df.to_dict('records')
                     self.cfs[row] = {
                         'original': self.res.cf_examples_list[0].test_instance_df.iloc[0],
                         'cfs': self.res.cf_examples_list[0].final_cfs_df
@@ -157,7 +158,7 @@ class DiCeReport:
         :return:
         """
         print(
-            f"""{len(self.instances_to_change) / len(self.model.X_test)}% of instances of undesired class in the dataset""")
+            f"""{round(len(self.instances_to_change) * 100 / len(self.model.X_test), 2)}% of instances of undesired class in the dataset""")
         print(f"""{len(self.subset)} instances of undesired class analyzed""")
         print(f"""{len(self.cfs) * 100 / len(self.subset)}% of successfull explanations""")
         print(f"""{self.c * 100 / len(self.subset)}% of programming package errors""")
@@ -206,11 +207,16 @@ class DiCeReport:
 
 class LimeReport:
 
-    def __init__(self, model, desired_class, slime=True):
+    def __init__(self, model, desired_class, slime=True, features_to_vary=None):
 
         self.model = model
         self.slime = slime
         self.desired_class = desired_class
+
+        self.features_to_vary = features_to_vary
+        if self.features_to_vary is None:
+            self.features_to_vary = self.model.names
+
         self.instances_to_change = [i for i, x in
                                     enumerate((model.grid_pipe_lgbm.predict(model.X_test) == desired_class))
                                     if x]
@@ -222,19 +228,18 @@ class LimeReport:
         """
         if self.slime:
             self.exp = sLimeTabularExplainer(self.model.X_train.values,
-                                       mode='classification',
-                                       feature_names=self.model.X_train.columns,
-                                       discretize_continuous=False,
-                                       feature_selection='lasso_path',
-                                       sample_around_instance=True)
+                                             mode='classification',
+                                             feature_names=self.model.X_train.columns,
+                                             discretize_continuous=False,
+                                             feature_selection='lasso_path',
+                                             sample_around_instance=True)
         else:
             self.exp = LimeTabularExplainer(self.model.X_train.values,
-                                        mode='classification',
-                                        feature_names=self.model.X_train.columns,
-                                        discretize_continuous=False)
+                                            mode='classification',
+                                            feature_names=self.model.X_train.columns,
+                                            discretize_continuous=False)
 
-
-    def get_exp(self, instance_id=None, features_to_vary=None, printout=False):
+    def get_exp(self, instance_id=None, printout=False):
         """
         Explain one particular instance
         :param features_to_vary:
@@ -245,18 +250,16 @@ class LimeReport:
             instance_id = random.choice(self.instances_to_change)
         if self.slime:
             self.one_exp = self.exp.slime(self.model.X_test.iloc[instance_id].values,
-                                self.model.grid_pipe_lgbm.predict_proba,
-                                num_features=len(self.model.X_train.columns), num_samples=1000,
-                                n_max=10000, alpha=0.05)
+                                          self.model.grid_pipe_lgbm.predict_proba,
+                                          num_features=len(self.model.X_train.columns), num_samples=1000,
+                                          n_max=10000, alpha=0.05)
         else:
             self.one_exp = self.exp.explain_instance(self.model.X_test.iloc[instance_id],
-                                                 self.model.grid_pipe_lgbm.predict_proba,
-                                                 num_features=len(self.model.X_train.columns))
+                                                     self.model.grid_pipe_lgbm.predict_proba,
+                                                     num_features=len(self.model.X_train.columns))
 
-        if features_to_vary is None:
-            features_to_vary = self.model.names
         self.lime_pred = pd.DataFrame([t for t in self.one_exp.as_list()
-                                       if any(f == t[0] for f in features_to_vary)],
+                                       if any(f == t[0] for f in self.features_to_vary)],
                                       columns=['feature', 'lime_coef'])
         self.lime_pred['instance_id'] = instance_id
         if self.desired_class == 0:
@@ -265,7 +268,7 @@ class LimeReport:
         if printout:
             self.one_exp.show_in_notebook(show_table=True, show_all=False)
 
-    def evaluate_dataset(self, cfs: dict = None, n=None, n_exps=5, features_to_vary=None, save=False, name=''):
+    def evaluate_dataset(self, cfs: dict = None, n=None, n_exps=5, save=False, name=''):
         """
         Generate lime explanations for several instances of a dataset
         :param cfs: dict of counterfactuals if present
@@ -283,11 +286,11 @@ class LimeReport:
         self.limeexps = []
         for row in tqdm(self.subset):
             if self.slime:
-                self.get_exp(instance_id=row, features_to_vary=features_to_vary)
+                self.get_exp(instance_id=row)
                 self.limeexps.append(self.lime_pred)
             else:
                 for i in range(n_exps):
-                    self.get_exp(instance_id=row, features_to_vary=features_to_vary)
+                    self.get_exp(instance_id=row)
                     self.limeexps.append(self.lime_pred)
         if self.slime:
             self.allcoefs = pd.concat(self.limeexps, axis=0).sort_values(by=['instance_id', 'feature'])
@@ -328,44 +331,108 @@ class LimeReport:
         plt.legend([], [], frameon=False)
 
 
+class ShapReport:
+
+    def __init__(self, model, desired_class, features_to_vary=None):
+        self.model = model
+        self.desired_class = desired_class
+
+        self.features_to_vary = features_to_vary
+        if self.features_to_vary is None:
+            self.features_to_vary = self.model.names
+
+        self.instances_to_change = [i for i, x in
+                                    enumerate((model.grid_pipe_lgbm.predict(model.X_test) == desired_class))
+                                    if x]
+
+    def create_explainer(self):
+        shap.initjs()
+        self.explainer = shap.TreeExplainer(self.model.grid_pipe_lgbm.best_estimator_)
+        self.shap_values = self.explainer.shap_values(self.model.X_test)
+
+
 class Comparator:
 
     def __init__(self, model, desired_class=0, dice=None, shap=None):
-
         self.dice = dice
-        self.lime = LimeReport(model, desired_class)
+        self.features_to_vary = [col for col in dice.alldiffs.columns if 'F_' in col]
+        self.lime = LimeReport(model, desired_class, slime=True, features_to_vary=self.features_to_vary)
         self.shap = shap
+        self.model = model
 
-    def compare_dice_lime(self, features_to_vary):
+    def compare_dice_slime(self):
+
         self.lime.create_explainer()
-        self.lime.evaluate_dataset(cfs=self.dice.cfs, features_to_vary=features_to_vary)
+        self.lime.evaluate_dataset(cfs=self.dice.cfs)
 
-        tmp = self.lime.aggbyidfeat.drop(columns=['std']).reset_index()
-        self.tmp = tmp.pivot_table(index='instance_id', columns='feature', values='mean').reset_index()
+        tmp = self.lime.aggbyidfeat.reset_index()
+        self.tmp = tmp.pivot_table(index='instance_id', columns='feature', values='lime_coef').reset_index()
 
         self.wide = self.dice.alldiffs.merge(self.tmp, how='left', on='instance_id',
-                        suffixes=['_dice', '_lime'])
+                                             suffixes=['_dice', '_lime'])
 
         long_dice = pd.melt(self.dice.alldiffs.reset_index(),
                             id_vars=['instance_id', 'index'],
-                            value_vars=features_to_vary,
+                            value_vars=self.features_to_vary,
                             var_name='feature', value_name='dice_diff')
 
-        self.long = long_dice.merge(self.lime.aggbyidfeat.drop(columns=['std']),
-                        how='left', on=['instance_id','feature'])\
+        self.long = long_dice.merge(self.lime.aggbyidfeat,
+                                    how='left', on=['instance_id', 'feature']) \
             .dropna(how='any', axis=0).sort_values(['instance_id', 'index'])
-        self.long['one_sign'] = ~((self.long['dice_diff'] > 0) ^ (self.long['mean'] > 0))
+        self.long['one_sign'] = ~((self.long['dice_diff'] > 0) ^ (self.long['lime_coef'] > 0))
 
-        self.mean_sign_correspondence = self.long.groupby('feature')[['one_sign']].mean().sort_values('one_sign')
-        self.mean_sign_correspondence.columns = ['% of corresponding signs']
-        print('Share of cases where mean LIME coefficient sign corresponds to DiCE suggestion sign:')
-        print(self.mean_sign_correspondence)
+        self.sign_correspondence = self.long.groupby('feature')[['one_sign']].mean().sort_values('one_sign')
+        self.sign_correspondence.columns = ['% of equal signs']
+        print('Share of cases where  SLIME coefficient sign is equal to DiCE suggestion sign:')
+        print(self.sign_correspondence.round(4) * 100)
 
-    def compare_instance(self, instance_id=None, features_to_vary=None):
-
+    def compare_instance(self, instance_id=None):
         if instance_id is None:
             instance_id = random.choice(list(self.dice.cfs.keys()))
 
-        self.lime.get_exp(instance_id=instance_id,
-                          features_to_vary=features_to_vary, printout=True)
+        # LIME
+        self.lime.get_exp(instance_id=instance_id, printout=False)
 
+        fig = go.Figure(go.Bar(
+            x=self.lime.lime_pred.feature,
+            text=list(self.lime.lime_pred.lime_coef.round(4).astype(str).values),
+            y=list(self.lime.lime_pred.lime_coef.round(4).values),
+        ))
+
+        fig.update_layout(height=400, width=800,
+                          title=f"""SLIME results for instance {instance_id}""",
+                          showlegend=False)
+
+        fig.show()
+
+        # DiCE
+        original = self.dice.cfs[instance_id]['original'][self.features_to_vary].values
+        cfs = self.dice.cfs[instance_id]['cfs'][self.features_to_vary].iloc[0].values
+        mask = (original != cfs)
+
+        changed_features = list(compress(self.features_to_vary, mask))
+        original = list(compress(original, mask))
+        cfs = list(compress(cfs, mask))
+
+        fig = make_subplots(rows=1, cols=len(changed_features), subplot_titles=changed_features)
+
+        for c in range(len(changed_features)):
+            y_rc = [original[c], cfs[c] - original[c], cfs[c]]
+            fig.add_trace(
+                go.Waterfall(x=['original', 'difference', 'counterfactual'], y=y_rc,
+                             orientation="v", name=self.features_to_vary[c],
+                             measure=["relative", "relative", "total"],
+                             text=[round(val, 3) for val in y_rc],
+                             connector={"line": {"color": "rgb(63, 63, 63)"}}),
+                row=1, col=c + 1)
+
+        fig.update_layout(height=300, width=800,
+                          title_text=f"""DiCE results for instance {instance_id}""",
+                          showlegend=False)
+        fig.show()
+
+        # SHAP
+        # shap.force_plot(self.shap.explainer.expected_value[1],
+        #                 self.shap.shap_values[1][instance_id, :],
+        #                 self.model.X_test.iloc[instance_id, :])
+        # plt.show()
